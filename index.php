8049db0bbe263fb2cfbddf8ee4f9fb7074f75632
Molka <molkabenameur6@gmail.com>
2021-02-27 20:09:37 +0100

    second

---
+++ index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <title>ECJE </title>
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/business-casual.min.css" rel="stylesheet">
+
+
+
+<!-- ------------- -->
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400&display=swap" rel="stylesheet">
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet"> -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+  <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet"> -->
+  <!-- Template Stylesheet -->
+  <link href="css/style.css" rel="stylesheet">
+
+
+</head>
+
+<body>
+
+
+<?php
+require 'header.php' ; 
+?>
+
+<!-- Service Start -->
+<div class="service">
+    <div class="container">
+
+
+
+
+        <div class="section-header">
+            <h2>Our Formation</h2>
+        </div>
+
+
+        <div class="row">
+            <div class="col-lg-3 col-md-6">
+                <div class="service-item">
+                    <img src="img/service-1.jpg" alt="Service">
+                    <h3>Flour Cleaning</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet elit. Phasellus nec pretium mi. Curabitur facilisis ornare
+                    </p>
+                    <a class="btn" href="">Learn More</a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <div class="service-item">
+                    <img src="img/service-2.jpg" alt="Service">
+                    <h3>Glass Cleaning</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet elit. Phasellus nec pretium mi. Curabitur facilisis ornare
+                    </p>
+                    <a class="btn" href="">Learn More</a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <div class="service-item">
+                    <img src="img/service-3.jpg" alt="Service">
+                    <h3>Carpet Cleaning</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet elit. Phasellus nec pretium mi. Curabitur facilisis ornare
+                    </p>
+                    <a class="btn" href="">Learn More</a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <div class="service-item">
+                    <img src="img/service-4.jpg" alt="Service">
+                    <h3>Toilet Cleaning</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet elit. Phasellus nec pretium mi. Curabitur facilisis ornare
+                    </p>
+                    <a class="btn" href="">Learn More</a>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6">
+                <div class="service-item">
+                    <img src="img/service-4.jpg" alt="Service">
+                    <h3>Toilet Cleaning</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet elit. Phasellus nec pretium mi. Curabitur facilisis ornare
+                    </p>
+                    <a class="btn" href="">Learn More</a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <div class="service-item">
+                    <img src="img/service-4.jpg" alt="Service">
+                    <h3>Toilet Cleaning</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet elit. Phasellus nec pretium mi. Curabitur facilisis ornare
+                    </p>
+                    <a class="btn" href="">Learn More</a>
+                </div>
+            </div>
+
+
+
+            
+        </div>
+    </div>
+</div>
+<!-- Service End -->
+
+
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+
+
+
+
+  <?php
+require 'footer.php' ; 
+?>
+
+
+</body>
+
+</html>
